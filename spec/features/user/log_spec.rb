2d8d40f16f415fe6ca3ec26d1d70cce34fb2b7ea
sasloan <sebastiansloan94@gmail.com>
2020-03-24 19:48:28 -0600

    Created user new spec

--- spec/features/user/log_spec.rb
+++ spec/features/user/log_spec.rb
@@ -58,19 +58,5 @@ RSpec.describe 'As a User' do
   end
 
 
-	it 'I can not make two users with the same log in' do
-		user = User.create(email: 'user@email.com', password: 'password', first_name:'Jim', role: 0)
 
-		visit '/users/new'
-
-		fill_in "user_email", with: "user@email.com"
-		fill_in "user_first_name", with: "Users"
-		fill_in "user_last_name", with: "Userson"
-		fill_in "user_password", with: "password1"
-		fill_in "user_password_confirmation", with: "password1"
-
-		click_on "Create Account"
-
-		expect(page).to have_content("Username already exists")
-	end
 end
