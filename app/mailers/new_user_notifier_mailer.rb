d67bec2d8883fdcb92d97cf7a3b76b596765a79b
sasloan <sebastiansloan94@gmail.com>
2020-03-30 19:29:36 -0600

    set up email views

--- app/mailers/new_user_notifier_mailer.rb
+++ app/mailers/new_user_notifier_mailer.rb
@@ -1,2 +1,8 @@
 class NewUserNotifierMailer < ApplicationMailer
+	def inform(info, user_email)
+		@user = info[:user]
+		@message = info[:message]
+		@new_user = info[:new_user]
+		mail(to: user_email, subject: "Activate your new account.")
+	end
 end
