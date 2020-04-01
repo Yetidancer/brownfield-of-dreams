52a3b0af4f817b99351b1240b19af80524f9ca80
sasloan <sebastiansloan94@gmail.com>
2020-04-01 14:26:03 -0600

    Delete duplicate spec

--- spec/features/user/can_see_github_repos_spec.rb
+++ spec/features/user/can_see_github_repos_spec.rb
@@ -30,60 +30,6 @@ feature 'user can see their repos' do
     expect(page).to have_link("D_and_Z_Pet_Place")
   end
 
-  it 'user logs in and visits dashboard and does not see the previous log ins repos', :vcr do
-
-    user = create(:user, email: "person@example.com", first_name: "Cheese", last_name: "Gecko", password: "password", token: ENV["YET_GITHUB_TOKEN"], github_username: "Yetidancer")
-    user2 = create(:user, email: "person1@example.com", first_name: "Cheesey", last_name: "Geckoy", password: "password1", token: ENV["SAS_GITHUB_TOKEN"], github_username: "sasloan")
-
-    visit '/'
-
-    click_on "Sign In"
-
-    expect(current_path).to eq(login_path)
-
-    fill_in 'session[email]', with: user.email
-    fill_in 'session[password]', with: user.password
-
-    click_on 'Log In'
-
-    expect(current_path).to eq(dashboard_path)
-
-    visit '/dashboard'
-
-    expect(page).to have_content("Github")
-    expect(page).to have_css(".repo", count:5)
-
-    within(first(".repo")) do
-      expect(page).to have_link("monster_shop_1911")
-    end
-
-		click_on "Log Out"
-
-		visit '/'
-
-		expect(current_path).to eq('/')
-
-		click_on "Sign In"
-
-    expect(current_path).to eq(login_path)
-
-    fill_in 'session[email]', with: user2.email
-    fill_in 'session[password]', with: user2.password
-
-    click_on 'Log In'
-
-    expect(current_path).to eq(dashboard_path)
-
-    visit '/dashboard'
-
-    expect(page).to have_content("Github")
-    expect(page).to have_css(".repo", count:5)
-    expect(page).to_not have_link("D_and_Z_Pet_Place")
-    within("#follower-Yetidancer") do
-      expect(page).to have_content("button")
-    end
-  end
-
   feature 'As a user that has not logged into Github yet' do
   	before :each do
   		OmniAuth.config.mock_auth[:github] = nil
