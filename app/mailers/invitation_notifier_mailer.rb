0529cf751996ba13ad3b7741fb2f37c85beb7fe4
sasloan <sebastiansloan94@gmail.com>
2020-04-01 15:34:27 -0600

    Add infrom method to mailer

--- app/mailers/invitation_notifier_mailer.rb
+++ app/mailers/invitation_notifier_mailer.rb
@@ -1,2 +1,6 @@
 class InvitationNotifierMailer < ApplicationMailer
+	def inform(invitation)
+    @recipient = invitation
+    mail(to: invitation.email, subject: "#{invitation.sender_name} is inviting you to join Brownfield of Dreams.")
+  end
 end
